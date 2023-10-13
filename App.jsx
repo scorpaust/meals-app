a9d4d082b58a076216fb8971fbdf931c78bc14a1
Dinis <scorpaust@hotmail.com>
2023-10-13 01:07:28 +0100

    Drawer Navigation

--- App.jsx
+++ App.jsx
@@ -1,13 +1,44 @@
 /* eslint-disable no-unused-vars */
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
-
 import CategoriesScreen from './screens/CategoriesScreen';
+import FavoritesScreen from './screens/FavoritesScreen';
 import MealDetailScreen from './screens/MealDetailScreen';
 import MealsOverviewScreen from './screens/MealsOverviewScreen';
+import { Ionicons } from '@expo/vector-icons';
 
 const Stack = createNativeStackNavigator();
+
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+  return <Drawer.Navigator screenOptions={{
+    headerStyle: {
+      backgroundColor: '#351401',
+    },
+    headerTintColor: 'white',
+    sceneContainerStyle: {
+      backgroundColor: '#3f2f25',
+    },
+    drawerContentStyle: {
+      backgroundColor: '#351401'
+    },
+    drawerInactiveTintColor: 'white',
+    drawerActiveTintColor: '#351401',
+    drawerActiveBackgroundColor: '#e4baa1'
+  }}>
+    <Drawer.Screen name="Categories" component={CategoriesScreen} options={{
+      title: 'All Categories',
+      drawerIcon: ({color, size}) => (<Ionicons name="list" color={color} size={size} />)
+    }} />
+    <Drawer.Screen name="Favorites" component={FavoritesScreen} options={{
+      title: 'Favorites',
+      drawerIcon: ({color, size}) => (<Ionicons name="star" color={color} size={size} />)
+    }} />
+  </Drawer.Navigator>
+}
 
 export default function App() {
   return (
@@ -25,14 +56,17 @@ export default function App() {
             },
           }}>
           <Stack.Screen
-            name="MealsCategories"
-            component={CategoriesScreen}
+            name="Drawer"
+            component={DrawerNavigator}
             options={{
               title: 'All Categories',
+              headerShown: false
             }}
           />
           <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
-          <Stack.Screen name="MealDetail" component={MealDetailScreen} />
+          <Stack.Screen name="MealDetail" component={MealDetailScreen} options={{
+            title: 'About the Meal'
+          }} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
--- screens/MealDetailScreen.jsx
+++ screens/MealDetailScreen.jsx
@@ -1,4 +1,6 @@
-import { Text, View, Image, StyleSheet, ScrollView } from 'react-native';
+import { useLayoutEffect } from 'react';
+import { Text, View, Image, StyleSheet, ScrollView, Button } from 'react-native';
+import IconButton from '../components/IconButton';
 import List from '../components/MealDetail/List';
 import Subtitle from '../components/MealDetail/Subtitle';
 import MealDetails from '../components/MealDetails';
@@ -9,6 +11,18 @@ function MealDetailScreen({ route, navigation }) {
   const mealId = route.params.mealId;
 
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
+  function headerButtonPressHandler() {
+      console.log("Pressed")
+  }
+
+  useLayoutEffect(() => {
+      navigation.setOptions({
+        headerRight: () => {
+            return <IconButton icon='star' color='white' onPress={headerButtonPressHandler} />
+        }
+      })
+  }, [navigation, headerButtonPressHandler]);
 
   return (
     <ScrollView style={styles.rootContainer}>
